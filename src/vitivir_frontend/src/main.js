ade25f6b1d63df7e1e83e75d1dc8324ad27c4227
Eden Darnige <edendarnige@gmail.com>
2020-04-27 14:14:49 +0200

    Remove bootstrap

--- src/vitivir_frontend/src/main.js
+++ src/vitivir_frontend/src/main.js
@@ -1,23 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from './router'
-import BootstrapVue from "bootstrap-vue";
+//import BootstrapVue from "bootstrap-vue";
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
 import MaterialKit from "./plugins/material-kit";
-// import 'vue-material-design-icons/styles.css';
 
 Vue.config.productionTip = false;
-Vue.use(BootstrapVue);
 
 Vue.use(MaterialKit);
-
-
-
-import "bootstrap/dist/css/bootstrap.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
 
 
 const NavbarStore = {
