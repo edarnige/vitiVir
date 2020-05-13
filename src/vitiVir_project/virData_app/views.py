b97bd15ac8c631a5a0e6aecfc58c74435dae8118
Eden Darnige <edendarnige@gmail.com>
2020-05-13 15:59:58 +0200

    Backend filtering with get_queryset override and mongo functions

--- src/vitiVir_project/virData_app/views.py
+++ src/vitiVir_project/virData_app/views.py
@@ -5,13 +5,6 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authentication import TokenAuthentication
 #from rest_framework.response import Response
 
-#from rest_framework.filters import SearchFilter, OrderingFilter
-#from django_filters import rest_framework as filters
-#from django_filters.rest_framework import FilterSet, filters
-#import rest_framework_filters as filters
-#import django_filters
-
-
 from .serializers import EntrySerializer
 from .models import Entry
 from .filters import EntrySearchFilter
@@ -21,42 +14,120 @@ from rest_framework import pagination
 from rest_framework_csv.renderers import CSVRenderer
 from django.db.models import Q
 
-from bson.json_util import dumps
+#import re
+
 
 
 class EntryListView(viewsets.ModelViewSet):
     ''' Entry API list view '''
     
     queryset = Entry.objects.all()
+    #queryset = Entry.objects.mongo_find({'sample':'SRR10518885'})
+    #print(">>>>>>",queryset)
+
     serializer_class = EntrySerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    #filter_class = EntrySearchFilter
     pagination_calss= pagination.PageNumberPagination
 
-    # filter_backends = (filters.DjangoFilterBackend,)
-    # filter_fields = ('virus_type','sample') 
+
+    # cursor = Entry.objects.mongo_find({'blastx.organism':{'$regex':'vitis', '$options': 'i'}})
+    # print(cursor)
+    # count =0
+    # entry_ids=[]
+    # for i in cursor:
+    #     count+=1
+    #     entry_ids.append(i['entry_id'])
+
+    # print(count)
+    # print(entry_ids)
+
+    # queryset = Entry.objects.filter(entry_id__in=entry_ids)
+    # print(">>>2>>>",queryset)
+
 
     def get_queryset(self):
-        fields = ['query_length', 'organism']
-        queryset = self.queryset
+        fields = ['sample', 'host_organism', 'virus_type', 'taxonomy', 'description','verified','exclude']
+        mongo_query = []
+        mongo_results = ''
+        entry_ids = []
+
         for field in fields:
-            
-            #Get from url ?organism=Vitis vinifera
             if field in self.request.GET:
                 value = self.request.GET.get(field)
-                if field == 'query_length':
-                    queryset = queryset.filter(blastx__exact={'query_length': int(value)})
-                elif field == 'organism':
-                    queryset = queryset.filter(blastx__exact={'organism': value})
+                if field == 'sample':
+                    mongo_query.append({'sample':{'$regex':value, '$options': 'i'}})
+                if field == 'host_organism':
+                    mongo_query.append({'host_organism':{'$regex':value, '$options': 'i'}})
+                if field == 'virus_type':
+                    mongo_query.append({'virus_type':{'$regex':value, '$options': 'i'}})
+                if field == 'taxonomy':
+                    mongo_query.append({'blastx.taxonomy':{'$regex':value, '$options': 'i'}})
+                if field == 'description':
+                    mongo_query.append({'blastrps.description':{'$regex':value, '$options': 'i'}})
+                if field == 'verified':
+                    if value == 'true':
+                        value = True
+                        mongo_query.append({'verified':value})
+                # if field == 'exclude':
+                #     mongo_query.append({'organism':{'$not':{'$regex':value, '$options': 'i'}}})
+                #dates       
+        
+        for entry in mongo_results:
+            entry_ids.append(entry['entry_id'])
+        
+        if mongo_query:
+            mongo_results = Entry.objects.mongo_find({'$and': mongo_query})
+            for entry in mongo_results:
+                entry_ids.append(entry['entry_id'])
+            queryset = Entry.objects.filter(entry_id__in = entry_ids)
+        else:
+            queryset = Entry.objects.all()
 
-        # ordering not working
-        ordering = self.request.GET.get('ordering')
-
-        if ordering:
-            queryset = queryset.order_by(ordering)
-        print(queryset.query)
         return queryset
+
+
+
+
+    # def get_queryset(self):
+    #     fields = ['query_length', 'organism', 'sample']
+    #     queryset = self.queryset
+    #     #query={'blastx__startswith':{'organism':'Vitis vinifera'}}
+    #     for field in fields:
+            
+    #         #Get from url ?organism=Vitis vinifera
+    #         if field in self.request.GET:
+    #             value = self.request.GET.get(field)
+    #             if field == 'query_length':
+    #                 #queryset = queryset.filter(blastx__exact={'query_length': int(value)})
+
+    #             elif field == 'organism':
+    #                 queryset = queryset.mongo_find({'blastx':{'organism':{'$regex':"*"+value+"*"}}})
+    #             # elif field == 'sample':
+    #             #     #queryset = queryset.filter(sample__icontains=value)
+    #             #     queryset = queryset.mongo_find({'sample':{'$regex':"*"+value+"*"}})
+
+
+    #     # ordering not working
+    #     ordering = self.request.GET.get('ordering')
+
+    #     if ordering:
+    #         queryset = queryset.order_by(ordering)
+        
+    #     #queryset = queryset.filter(**query)
+    #     print(queryset.query)
+    #     return queryset
+
+        #     index = [i for i in queryset.mongo_aggregate([
+        #     {
+        #         '$match': {
+        #             'headline': self.kwargs['path']
+        #         }
+        #     },
+        # ])]
+
+
+
 
 class EntryListCSVExportView(viewsets.ModelViewSet):
     '''Make CSV from Entry list results'''
@@ -78,6 +149,20 @@ class EntryListCSVExportView(viewsets.ModelViewSet):
 
 
     '''SCRATCH - in progress'''
+
+    #Filtering idea
+#     $(".filterInput").each(function() {
+#     var column_name = $(this).attr("name")
+#     filters[column_name] = $(this).val();
+# });
+
+# query = {}
+# for k, v in filters.items():
+#     if v:
+#         query[k + '__icontains'] = v
+
+# result = model.objects.filter(**query).order_by(order_by).skip(page_size*(page-1)).limit(page_size).only(*attr_names)
+
 
     # @action(methods=['get'], detail=False)
     # def get_queryset(self, sample, host_organism, virus_type, verified, exclude_vitis, order):
