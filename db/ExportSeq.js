4237d3a7b125e9dcc8dc996a6e91a1bdf959e0bc
Eden Darnige <edendarnige@gmail.com>
2020-06-17 14:46:39 +0200

    Space after ID

--- db/ExportSeq.js
+++ db/ExportSeq.js
@@ -1,7 +1,7 @@
 //get sequences for virseq db
 db.virData_app_entry.find({'blastx.sequence':{'$exists':'true'}},
     {'blastx.sequence':1, 'blastx.accession':1, 'entry_id':1, "blastx.description":1,"query_id":1}).forEach(function(entry){ //sample and blastx.accession:1?
-    print(">"+entry.entry_id.hex().toString()+"|"+entry.blastx.accession+"|"+entry.blastx.description+"|"+entry.query_id+
+    print(">"+entry.entry_id.hex().toString()+" |"+entry.blastx.accession+"|"+entry.blastx.description+"|"+entry.query_id+
       "\n"+entry.blastx.sequence);
   });
 
