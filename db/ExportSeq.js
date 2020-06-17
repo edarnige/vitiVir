5fe0f442f62860246f73f569dabb781b26bb71d9
Eden Darnige <edendarnige@gmail.com>
2020-06-17 14:40:34 +0200

    Reorder description with ID first

--- db/ExportSeq.js
+++ db/ExportSeq.js
@@ -1,8 +1,8 @@
 //get sequences for virseq db
 db.virData_app_entry.find({'blastx.sequence':{'$exists':'true'}},
     {'blastx.sequence':1, 'blastx.accession':1, 'entry_id':1, "blastx.description":1,"query_id":1}).forEach(function(entry){ //sample and blastx.accession:1?
-    print(">"+entry.blastx.accession+"|"+entry.blastx.description+"|"+entry.query_id+"|"+
-      entry.entry_id.hex().toString()+"\n"+entry.blastx.sequence);
+    print(">"+entry.entry_id.hex().toString()+"|"+entry.blastx.accession+"|"+entry.blastx.description+"|"+entry.query_id+
+      "\n"+entry.blastx.sequence);
   });
 
 
