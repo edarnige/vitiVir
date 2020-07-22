b5f0fc5e53d9d1960169d40eb7b6af6742f9db40
Eden Darnige <edendarnige@gmail.com>
2020-07-22 11:14:30 +0200

    Fix request error on existing account

--- src/vitivir_frontend/src/views/Signup.vue
+++ src/vitivir_frontend/src/views/Signup.vue
@@ -6,24 +6,19 @@
           <div
             class="md-layout-item md-size-33 md-small-size-66 md-xsmall-size-100 md-medium-size-40 mx-auto"
           >
-            <!-- <login-card header-color="primary"> -->
-            <!-- <login-card class="md-card-header-primary"> -->
             <login-card>
               <h4 slot="title" class="card-title">Sign up</h4>
               <p slot="description" class="description">A request to create an account will be sent. Check your email for updates.</p>
 
               <md-field class="md-form-group" slot="inputs">
-                <!-- <md-icon>email</md-icon> -->
                 <label>Email...</label>
                 <md-input id="email" v-model="email" type="email" required></md-input>
               </md-field>
               <md-field class="md-form-group" slot="inputs">
-                <!-- <md-icon>lock_outline</md-icon> -->
                 <label>Password...</label>
                 <md-input id="password" v-model="password" type="password" required></md-input>
               </md-field>
               <md-field class="md-form-group" slot="inputs">
-                <!-- <md-icon>face</md-icon> -->
                 <label>Confirm password...</label>
                 <md-input id="passwordConfirmed" v-model="passwordConfirmed" type="password" required></md-input>
               </md-field>
@@ -81,41 +76,38 @@ export default {
         })
         .then(res => {
           console.log("signed up", res),
-          this.$router.push('/login')
+          
+          //Send email to request account, suepruser must switch is_active on 
+          axios.post(`${process.env.VUE_APP_API_HOST}/api/request_account/`, data)
+          .then(res=>{
+            console.log(res)
+            alert('Request sent');
+          })
+
+          .catch(err =>{
+            console.log(err);
+            alert("Unable to send request")
+            });
         })
+
         .catch(err =>{
           alert("Unable to request account with these credentials")
           console.log(err);
         })
-
-        //Send email to request account, suepruser must switch is_active on 
         const data = {
           'email': this.email,
         }
-        axios.post(`${process.env.VUE_APP_API_HOST}/api/request_account/`, data)
-        .then(res=>{
-          console.log(res)
-          alert('Request sent');
-        })
-        .catch(err =>{
-          console.log(err);
-          alert("Unable to send request")
-          });
       }
 
       else{
         alert("Passwords do not match")
         console.log("Passwords do not match")
       }
-
-
-      
-
     },
   }
 
   };
-// };
+
 </script>
 
 <style lang="css"></style>
